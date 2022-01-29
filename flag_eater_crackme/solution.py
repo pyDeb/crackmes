e4d3ebf2732072afdbae3838453a368999126750
Masoud Sadeghi <35397912+pyDeb@users.noreply.github.com>
2022-01-29 14:51:19 -0500

    Create solution.py

---
+++ flag_eater_crackme/solution.py
@@ -0,0 +1,13 @@
+six = "`"  # equivalent of 96 in ascii table 6*16
+thirty = "a" 
+thirtythree = "g"
+seven = "p"  # 7*16 
+three = "0"  # 3*16
+thirtytwo = "e" 
+five = "P"
+thirtyfive = "k"
+thirtysix = "m"
+
+
+print(six + six + thirty + thirtythree + seven * 2 + three + seven + thirtytwo + six + three + five + thirtyfive + three + five + seven * 3 + six + thirtysix + seven + \
+five + six * 2 + three + five + three + seven + thirty + seven)
